b41b31bff7de658f944e7d897aebc06834e973eb
DenMat <DenMat tu2Bgone@gmail.com>
2013-06-18 14:43:51 +1000

    initial commit

---
+++ partitions/mel.rb
@@ -0,0 +1,21 @@
+partition "drp" do
+  label "dev",  :address => "192.168.22.0/24"
+  label "qa",   :address => "192.168.23.0/27"
+  label "drp",  :address => "192.168.24.0/24"
+  label "prod", :address => "192.168.25.0/24"
+  label "www.abc.com", :address => "202.111.222.111"
+  label "mail.abc.com", :address => "202.111.222.112"
+  label "app1.local",     :address => "192.168.24.10"
+  label "mail.local",     :address => "192.168.24.11"
+
+  rewrite "drp web" do
+    ports 80
+    dnat  "www.abc.com" => "app1.local"
+  end
+  
+  rewrite "drp mail" do
+    ports 25 
+    dnat  "mail.abc.com" => "mail.local"
+  end
+
+end
